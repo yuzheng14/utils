0dee44ba814953f6be5e1a3248effffac48c7b13
yuzheng14 <422450117@qq.com>
2023-02-13 13:40:34 +0800

    test(index.test.ts): 增加导出测试

---
+++ test/index.test.ts
@@ -0,0 +1,17 @@
+import * as all from '@/index'
+import { describe, it, expect } from 'vitest'
+
+describe('函数导出测试', () => {
+  it('assignObjDefaultValue', () => {
+    expect(all.assignObjDefaultValue).toBeDefined()
+  })
+  it('deepCopyPropsOfObj', () => {
+    expect(all.deepCopyPropsOfObj).toBeDefined()
+  })
+  it('sleep', () => {
+    expect(all.sleep).toBeDefined()
+  })
+  it('throttle', () => {
+    expect(all.throttle).toBeDefined()
+  })
+})
