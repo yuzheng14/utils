60f43ee43f025f37f8324bb6f8f05fcd68bf8533
yuzheng14 <422450117@qq.com>
2023-02-14 11:12:38 +0800

    test(obj.test.ts): add test of assignObjValue

---
+++ test/obj.test.ts
@@ -0,0 +1,17 @@
+import { assignObjValue } from '@/obj'
+import { describe, expect, it } from 'vitest'
+
+interface User {
+  password: string
+  username: string
+}
+
+describe('obj.ts', () => {
+  it('assignObjValue', () => {
+    const user: User = {
+      password: '123456',
+      username: 'test',
+    }
+    expect(assignObjValue(user, { password: 'msun' })).toEqual({ password: 'msun', username: 'test' })
+  })
+})
