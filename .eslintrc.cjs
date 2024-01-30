fee981bcff17fada41fe03b8eeff5562a3699831
yuzheng14 <422450117@qq.com>
2024-01-30 21:01:07 +0800

    chore(@yuzheng14/utils): :wrench: eslint 修改 ignoredPatterns 字段

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  ignoredPatterns: ['.eslintrc.cjs'],
+  ignorePatterns: ['.eslintrc.cjs', 'node_modules', 'dist'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
