aa0d5dafec0c75af4d4b16a632146086adc72255
yuzheng14 <422450117@qq.com>
2024-06-07 22:26:37 +0800

    build: change build strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import { promisify } from 'util'
-import { ExecException, exec } from 'child_process'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
@@ -12,19 +10,7 @@ export default defineConfig({
       formats: ['es'],
       fileName: 'index',
     },
+    minify: false,
   },
-  plugins: [
-    // 在 plugin 中使用 vite 的 hook 生成 typescript 声明文件
-    {
-      name: 'generate-typescript-declaration',
-      async writeBundle() {
-        try {
-          await promisify(exec)('pnpm tsc')
-        } catch (error) {
-          const { stdout, stderr } = error as { stdout: string; stderr: string } & ExecException
-          this.error(`生成 typescript 声明文件失败：\n\tstdout: ${stdout}\n\tstderr:${stderr}`)
-        }
-      },
-    },
-  ],
+  plugins: [dts({ rollupTypes: true })],
 })
